d0c288690c2dd3292a97d87da513962c032754e2
nazzeya <n.baisakalova01@gmail.com>
2023-01-20 13:40:53 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'svg-angular-first';
+  elementOne: number = 0;
+  elementTwo: number = 13;
+  elementThree: number = 1;
+  elementFour: number = 4;
+  elementFive: number = 3;
+ 
+  check( element:number ) {
+    if(element==0) return true
+    else return false
+  }
+
 }
